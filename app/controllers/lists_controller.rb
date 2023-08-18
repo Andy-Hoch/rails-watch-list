bef451fa8c971f1515cfabd19d40cffcd6b1d01d
Andy Hoch <andreas.hoch@me.com>
2023-08-18 13:59:31 +0200

    Lets see

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,42 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show]
+  before_action :all_lists
+
+
+  def index
+    @lists = List.all
+    new
+  end
+
+  def show
+    @bookmarks = Bookmark.all
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def all_lists
+    @all_lists = List.all
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
